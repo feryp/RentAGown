8577e9c6a3772a5331110a4869c70cf8b391a9ff
Fery Pratama <37443445+feryp@users.noreply.github.com>
2021-01-18 14:31:24 +0700

    edit initialize id

--- app/src/main/java/com/example/rentagown/WriteReviewActivity.java
+++ app/src/main/java/com/example/rentagown/WriteReviewActivity.java
@@ -2,13 +2,19 @@ package com.example.rentagown;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.RatingBar;
 import android.widget.TextView;
 
-public class WriteReviewActivity extends AppCompatActivity {
+public class WriteReviewActivity extends AppCompatActivity implements View.OnClickListener{
 
+    ImageButton back;
+    Button btnSubmitReview;
     private RatingBar ratingBar;
 
 
@@ -17,6 +23,9 @@ public class WriteReviewActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_write_review);
 
+        //INIT VIEW
+        back = findViewById(R.id.im_back);
+        btnSubmitReview = findViewById(R.id.btn_submit_review);
         ratingBar = findViewById(R.id.rate);
 
         ratingBar.setOnRatingBarChangeListener(new RatingBar.OnRatingBarChangeListener() {
@@ -25,5 +34,22 @@ public class WriteReviewActivity extends AppCompatActivity {
                 Log.d("uuu",ratingBar.getRating()+"");
             }
         });
+
+        //SET LISTENER
+        back.setOnClickListener(this);
+        btnSubmitReview.setOnClickListener(this);
+    }
+
+    @SuppressLint("NonConstantResourceId")
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.im_back:
+                finish();
+                break;
+
+            case R.id.btn_submit_review:
+                break;
+        }
     }
 }
